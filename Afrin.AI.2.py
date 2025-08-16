78d46fcfc1cb1331e0143209337acc893a991b94
Afrin25007 <afrinusman.u.2007@gmail.com>
2025-08-16 17:57:57 +0530

    Add files via upload

---
+++ Afrin.AI.2.py
@@ -0,0 +1,40 @@
+def isSafe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+def solveNQueens(board, col):
+    if col >= len(board):
+        return True
+    for i in range(len(board)):
+        if isSafe(board, i, col):
+            board[i][col] = 1
+            if solveNQueens(board, col + 1):
+                return True
+            board[i][col] = 0
+    return False
+
+def printBoard(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            print(board[i][j], end=" ")
+        print()
+
+def solveQueens():
+    N = 8
+    board = [[0] * N for _ in range(N)]
+    if not solveNQueens(board, 0):
+        print("Solution does not exist")
+        return False
+    printBoard(board)
+    return True
+
+solveQueens()
+print("Afrin.U B.Tech AIDS")
